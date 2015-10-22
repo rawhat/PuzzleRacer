2ad7b1f5b3809f56199961e4b777b652b8dfa04a
Matt Walker <mjw355@drexel.edu>
2015-10-21 21:41:44 -0400

    Killplane integrated into week five demo. Checkpoint system largely works.

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -5,20 +5,44 @@ public class GameController : MonoBehaviour {
 
     // make this a dictionary later with reference ID for falling track pieces
     public GameObject[] trackPieceArray;
-
+    public GameObject player;
     private GameObject currentTrackPiece;
     private TrackController trackController;
+    private GameObject killPlane;
+    [SerializeField] private float totalDistanceTraveled;
+    [SerializeField] private float startingPosition;
+    private float checkpointIncrement;
+    [SerializeField] private float nextCheckpoint;
 
     void Start()
     {
+        totalDistanceTraveled = 0;
+        startingPosition = player.transform.position.z;
+        checkpointIncrement = 50f;
+        nextCheckpoint =  player.transform.position.z + checkpointIncrement;
         trackController = GameObject.FindObjectOfType<TrackController>();
+        killPlane = GameObject.Find("KillPlane");
     }
 
     void Update()
     {
+        IncrementDistanceTraveled();
         if (Input.GetKeyDown(KeyCode.Q))
         {
             trackController.SendMessage("SetNextTrackPiece", trackPieceArray[0]);
         }
+    }
+
+    void IncrementDistanceTraveled()
+    {
+        totalDistanceTraveled = player.transform.position.z - startingPosition;
+        if(totalDistanceTraveled >= nextCheckpoint)
+            IncrementCheckpoint();
+    }
+
+    void IncrementCheckpoint()
+    {
+        killPlane.GetComponent<KillPlaneBehave>().SetCheckpointPos(nextCheckpoint);
+        nextCheckpoint += checkpointIncrement;
     }
 }
--- Assets/Scripts/KillPlaneBehave.cs
+++ Assets/Scripts/KillPlaneBehave.cs
@@ -5,12 +5,13 @@ public class KillPlaneBehave : MonoBehaviour {
 
 	public GameObject killTarget; 
 	public GameObject startPiece;
-	[SerializeField] private Vector3 checkpointPos;
+	private Vector3 checkpointPos;
 	private Vector3 colliderSize;
 
 	// Use this for initialization
 	void Start () {
 		checkpointPos = killTarget.transform.position;
+		transform.position = new Vector3(killTarget.transform.position.x, killTarget.transform.position.y - 5, killTarget.transform.position.z);
 	}
 	
 	// Update is called once per frame
@@ -30,6 +31,7 @@ public class KillPlaneBehave : MonoBehaviour {
 	{
 		GameObject.Instantiate(startPiece, checkpointPos, Quaternion.identity);
 		killTarget.transform.position = new Vector3(checkpointPos.x, checkpointPos.y + 3, checkpointPos.z);
+		killTarget.transform.rotation = Quaternion.identity;
 
 	}
 
@@ -41,5 +43,14 @@ public class KillPlaneBehave : MonoBehaviour {
 	void FollowKillTarget()
 	{
 		transform.position = new Vector3(killTarget.transform.position.x, transform.position.y, killTarget.transform.position.z);
+	}
+
+	public Vector3 GetCheckpointPos()
+	{
+		return checkpointPos;
+	}
+	public void SetCheckpointPos(float zPos)
+	{
+		checkpointPos = new Vector3(checkpointPos.x, checkpointPos.y, zPos);
 	}
 }

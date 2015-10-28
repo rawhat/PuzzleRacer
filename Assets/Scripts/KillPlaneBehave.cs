7526b683725b571c30ca9783eccf7aa16c685236
Matt Walker <mjw355@drexel.edu>
2015-10-28 18:21:27 -0400

    Car no longer maintains momentum after falling.

--- Assets/Scripts/KillPlaneBehave.cs
+++ Assets/Scripts/KillPlaneBehave.cs
@@ -39,7 +39,10 @@ public class KillPlaneBehave : MonoBehaviour {
 		GameObject newTrack = (GameObject) GameObject.Instantiate(startPiece, checkpointPos, Quaternion.identity);
 		tc.SetCurrentTrackPiece(newTrack);
 		killTarget.transform.position = new Vector3(checkpointPos.x, checkpointPos.y + 3, checkpointPos.z);
+		killTarget.GetComponent<Rigidbody>().isKinematic = true;
 		killTarget.transform.rotation = Quaternion.identity;
+		killTarget.GetComponent<Rigidbody>().isKinematic = false;
+
 
 	}
 
